1c2f8a163def78d2c25fc4b8b859457eafcde776
Siddhesh Dhage <siddheshdhage@yahoo.com>
2022-05-08 11:38:53 +0100

    exploring some boiler plate stuff

---
+++ index.ts
@@ -0,0 +1,3 @@
+import {logger} from './services/utils';
+
+logger.silly('I am a silly log.');
---
+++ services/utils/logger.ts
@@ -0,0 +1,4 @@
+import { Logger } from "tslog";
+
+const log: Logger = new Logger();
+export const logger = log;
