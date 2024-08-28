f860f536fbd466b5698e85f4aff0c7e1695d10f2
Siddhesh Dhage <siddheshdhage@yahoo.com>
2024-08-28 15:07:54 +0100

    Kitchen Service

--- services/restaurant-example/fascade/kitchen-service/kitchen-service.ts
+++ services/restaurant-example/fascade/kitchen-service/kitchen-service.ts
@@ -1,5 +1,9 @@
 
-
+/**
+ * The Kitchen is a complicated service which ideally needs to maintain a balance of orders coming in and orders going out.
+ * It needs to be able to handle multiple orders at once and ensure that the orders are being cooked in the correct order.
+ * There is a life-cycle within itself that needs to be managed.
+ */
 export class KitchenService {
     constructor() { }
 
